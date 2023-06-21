e58238cac091ec5fe43ccf515fcc7214bdfac777
OBI CHIEMERIE     ("THALES") <obiwinner94@gmail.com>
2023-06-21 21:23:04 +0100

    god abegg

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,7 @@
--- This script prepares a MySQL server for the project
--- Create project developement database with the name : hbnb_dev_db
+-- prepares a MySQL server for the project
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Creating newer user named : hbnb_dev with all privileges on the db hbnb_dev_db
--- with the password : hbnb_dev_pwd if it dosen't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- Granting auto privileges to the new user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
--- Granting the SELECT privilege for the new user hbnb_dev in the db performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
