6a237d1ab5338c3e4bfc822348ab9180bc016cdb
OBI CHIEMERIE     ("THALES") <117930136+WINNER2028@users.noreply.github.com>
2023-07-06 09:48:43 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,6 +13,6 @@ echo "<!DOCTYPE html>
   </body>
 </html>" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R 204421:204421 /data
+chown -R ubuntu:ubuntu /data
 sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
